e3a8d726a9aa3fb464b441a9e8eff68e34d3c706
ahmedkhaled4219 <146847357+ahmedkhaled4219@users.noreply.github.com>
2023-11-14 08:55:59 +0200

    tt

---
+++ LAB3/CopyConstructor/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+
+class Stack{
+
+int Size;
+int t;
+int *arr;
+public:
+Stack(int _size){
+    t=-1;
+    Size=_size;
+ arr= new int[Size]{0};
+}
+
+Stack(Stack& s){
+t=s.t;
+Size=s.Size;
+arr= new int[Size]{0};
+for(int i=0;i<=t;i++){
+    arr[i]=s.arr[i];
+}
+}
+
+
+
+void push(int x){
+if(t!=Size-1){
+t++;
+arr[t]=x;
+}
+else{
+ cout<<"stack is full";
+}
+}//push
+
+void pop(){
+    int x=arr[t];
+if(t != -1){
+   cout<<x<<endl;
+    t--;
+
+}
+else{
+    cout<<"stack is empty"<<endl;
+}
+
+}//pop
+
+void display(){
+for(int i=0;i<=t;i++){
+
+    cout<<arr[i]<<endl;
+}
+}//display
+
+void DeepCopy(Stack s){
+s.pop();
+s.pop();
+s.pop();
+s.pop();
+}
+
+~Stack(){
+delete[] arr;
+}
+
+void ShallowCopy(Stack s);
+}; //end class
+
+void ShallowCopy(Stack s){
+s.pop();
+s.pop();
+s.pop();
+}
+
+int main()
+{
+
+Stack s1(5);
+//DeepCopy(s1);
+//s1.pop(); //empty
+s1.push(1);
+s1.push(5);
+s1.push(6);
+//ShallowCopy(s1);
+s1.push(8);
+//s1.DeepCopy(s1);//
+s1.pop();
+s1.pop();
+
+/*
+s1.push(6);
+s1.pop();//6
+s1.pop();//5
+s1.push(7);
+s1.pop();//7
+s1.pop();//1
+s1.pop();//Empty
+s1.push(7);
+s1.pop();//7
+//6571Empty7
+*/
+
+    return 0;
+}
